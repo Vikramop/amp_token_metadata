5a91d5a4eb26eebddf1243a860a13bca11913688
Vikramop <veervikramsingh132@gmail.com>
2025-05-21 13:08:28 +0530

    metadata updated

--- token_metadata.ts
+++ token_metadata.ts
@@ -4,49 +4,46 @@ import { base58 } from '@metaplex-foundation/umi/serializers';
 import {
   createSignerFromKeypair,
   signerIdentity,
+  publicKey,
 } from '@metaplex-foundation/umi';
 import {
   mplTokenMetadata,
   updateV1,
-  TokenStandard,
+  fetchMetadataFromSeeds,
 } from '@metaplex-foundation/mpl-token-metadata';
-import { percentAmount, publicKey } from '@metaplex-foundation/umi';
 
-// Parse the secret key from the .env variable
 const secretKeyString = process.env.SECRET_KEY;
 if (!secretKeyString) throw new Error('SECRET_KEY not found in .env');
 const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
 
-// Set up Umi and signer
 const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata());
 const keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(signer));
 
-// Your SPL Token mint address and metadata
-const mint = publicKey('6h5XJD9aArEqVyxAss98hdxJHoZLB4SzccVDdgEttRkf');
-const tokenMetadata = {
-  name: 'Amply',
-  symbol: 'AMP',
-  uri: 'https://raw.githubusercontent.com/Vikramop/amp_token_metadata/master/amply.json',
-};
+// Your SPL Token mint address
+const mint = publicKey('HYbi3JvAQNDawVmndhqaDQfBaZYzW8FxsAEpTae3mzrm');
 
-async function addMetadata() {
+// The new metadata URI (raw link to your updated JSON)
+const newUri =
+  'https://raw.githubusercontent.com/Vikramop/amp_token_metadata/master/amply.json';
+
+async function updateMetadata() {
+  // Fetch current metadata so we can preserve fields you don't want to change
+  const initialMetadata = await fetchMetadataFromSeeds(umi, { mint });
+
   const tx = await updateV1(umi, {
     mint,
     authority: umi.identity,
-    payer: umi.identity,
     updateAuthority: umi.identity,
-    name: tokenMetadata.name,
-    symbol: tokenMetadata.symbol,
-    uri: tokenMetadata.uri,
-    sellerFeeBasisPoints: percentAmount(0),
-    tokenStandard: TokenStandard.Fungible,
+    data: {
+      ...initialMetadata,
+      uri: newUri,
+    },
   }).sendAndConfirm(umi);
 
-  // Base58 encode the signature for Solana Explorer
   const txSig = base58.deserialize(tx.signature);
   console.log(`https://explorer.solana.com/tx/${txSig}?cluster=devnet`);
 }
 
-addMetadata();
+updateMetadata();
